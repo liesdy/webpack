453fd8d49f3dab29f7eee5e3a8bf201472017eef
洌水剑 <411476852@qq.com>
2021-01-16 10:37:14 +0800

    production

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import { cube } from './math.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 
 function component() {
-  let element = document.createElement('div');
-  var btn = document.createElement('button');
+  var element = document.createElement('pre');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-
-  btn.innerHTML = '点击这里，然后查看 console！';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
