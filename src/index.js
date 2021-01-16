c51b6fb67147287a755fa94ae85f47a240c5ef06
洌水剑 <411476852@qq.com>
2021-01-16 11:24:52 +0800

    codeSplitting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
-function component() {
-  let element = document.createElement('div');
+// function component() {
+//   let element = document.createElement('div');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+//   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   return element;
+// }
+// function getComponent() {
+async function getComponent() {
+  // return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+  //   var element = document.createElement('div');
+
+  //   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  //   return element;
+
+  // }).catch(error => 'An error occurred while loading the component');
+  var element = document.createElement('div');
+  const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
+
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: { 
+    index:'./src/index.js',
+    // another: './src/another-module.js'
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Production'
+    })
+  ],
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: 'all'
+  //   }
+  // }
 };
