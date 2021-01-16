e1eac6b8041f37442773a901caab04ed5f9254e3
洌水剑 <411476852@qq.com>
2021-01-16 10:23:26 +0800

    treeShaking

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import _ from 'lodash';
+import { cube } from './math.js';
 
 function component() {
-  let element = document.createElement('div');
+  var element = document.createElement('pre');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,9 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'production',
+  optimization: {
+    usedExports: true
   }
 };
