1ea06ae234bb4224a15569156da922e71b37a0c3
洌水剑 <411476852@qq.com>
2021-01-18 17:39:03 +0800

    pwa  这部分还不太清楚具体的效果

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import _ from 'lodash';
 import printMe from './print.js';
 
+// navigator.serviceWorkerContainer.register('service-worker.js').then(function(reg){
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    // navigator.serviceWorkerContainer.register('service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 function component() {
   let element = document.createElement('div');
   var btn = document.createElement('button');
