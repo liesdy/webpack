3f76ff8512c9d09aea98de20338f807ca714d250
洌水剑 <411476852@qq.com>
2021-01-18 08:59:05 +0800

    lazyLoading

--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 
-// function component() {
-//   let element = document.createElement('div');
-
-//   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//   return element;
-// }
-// function getComponent() {
-async function getComponent() {
-  // return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-  //   var element = document.createElement('div');
-
-  //   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  //   return element;
-
-  // }).catch(error => 'An error occurred while loading the component');
+function component() {
   var element = document.createElement('div');
-  const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
 
+  button.innerHTML = 'Click me and look at the console!';
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default;
+
+    print();
+  });
 
   return element;
-
 }
-
 // document.body.appendChild(component());
-getComponent().then(component => {
-  document.body.appendChild(component);
-})
+document.body.appendChild(component());
